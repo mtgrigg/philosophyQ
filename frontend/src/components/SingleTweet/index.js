9bc5efd21e73985b93f2bbb659d23ec73be66633
Max <maxtgrigg@gmail.com>
2022-02-17 10:16:28 -0800

    single tweet page working, proper comments display, tweets dedleted readily

--- frontend/src/components/SingleTweet/index.js
+++ frontend/src/components/SingleTweet/index.js
@@ -1,69 +1,79 @@
 import React from 'react'
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import {getSingleTweet} from '../../store/tweets';
 import { Link, useHistory  } from 'react-router-dom';
 import { getComments } from '../../store/comments';
 import { getUsers } from '../../store/users';
+import { getTweets } from '../../store/tweets';
 import CommentDisplay from '../CommentsDisplay';
 import UserDisplay from '../UsersDisplay';
-import EditTweet from '../EditTweet';
 import {deleteTweet} from '../../store/tweets'
-import CreateComment from '../CreateComment';
-// import user from '../../../../backend/db/models/user';
-import EditComment from '../EditComment';
-
+import EditTweet from '../EditTweet';
 
 
 
 const SingleTweet = ({tweetss}) => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const { tweetId } = useParams();
 
+    const {tweetId} = useParams();
+    console.log(tweetId, "THIS IS TWEETEYEDDD")
 
+  // grab comments
+    const comments = useSelector(state => state.comments)
+    const commentsArray = Object.values(comments);
 
-    const comments= useSelector(state => state.comments)
-    const commentsArray= Object.values(comments)
-    const filteredComments = commentsArray.filter(comment => comment.tweetId===tweetId)
+  // grab ALL users
+    const users = useSelector(state => state.users)
+    const usersArray = Object.values(users)
 
-    const users = useSelector(state=>state.users)
-    const usersArray= Object.values(users)
-    console.log(usersArray, 'THIS IS USERS ARRAY')
-    const userz= users[tweetId]
-    const filteredUsers= usersArray.filter(user => user.id === tweetss)
-
+  // grab ALL tweets
     const tweets = useSelector(state => state.tweet);
-    const tweetsArray= Object.values(tweets)
+    console.log(tweets, "THIS IS TWWEETTSSS")
+    const tweetsArray = Object.values(tweets)
+
+  const [targetTweet] = tweetsArray.filter(tweet => tweet.id === +tweetId);
+  console.log(targetTweet, "THIS IS TARGET TWEET")
+
+
+  const [targetUser] = usersArray.filter(user => user.id === targetTweet.userId);
+  console.log(targetUser, "THIS IS TARGET USSERRR")
+
+  function userCard(user) {
+
+    if (user === undefined) return;
+    const {username, imgUrl, bio} = user;
+    return (
+      <>
+        <div>
+          <div>{username}</div>
+          <div>{bio}</div>
+          <img src={imgUrl} alt='' />
+        </div>
+      </>
+    );
+  }
+
+
     const tweetz = tweets[tweetId]
-    const tweetUserId = tweetsArray.map(tweet => tweet.userId)
-
-const [loaded,setLoaded]= useState(false)
-
-    console.log(tweetsArray, 'THIS IS TWEETSARRAY')
+    const tweetUserId = tweetsArray.filter(tweet => tweet.userId)
 
     const currentUser = useSelector(state => state.session.user);
-const currentUserId = currentUser?.id
+    const currentUserId = currentUser?.id
 
-    // useEffect(()=>{
-
-    //     setTid(tweetId)
-    // }, [])
-
-    useEffect(()=>{
-
-
-          dispatch(getSingleTweet(tweetId));
-          dispatch(getComments(tweetId))
-        dispatch(getUsers())
-
-
+    useEffect(() => {
+      dispatch(getTweets());
+      dispatch(getUsers());
+      dispatch(getComments(tweetId));
     }, [dispatch])
 
-    const handleDeleteButton = async (e) => {
-        e.preventDefault();
-        const doDelete = await dispatch(deleteTweet(tweetz));
+
+
+        const handleDeleteButton = (e) => {
+
+        dispatch(deleteTweet(targetTweet));
 
           history.push("/tweets");
 
@@ -71,66 +81,31 @@ const currentUserId = currentUser?.id
 
       }
 
-
     return(
         <>
+
+<div >
+           <button  onClick={handleDeleteButton}>Delete Tweet</button>
+        </div>
+          {userCard(targetUser)}
+
        {/* {currentUserId === tweetz?.userId && <div>{currentUser?.username}</div>} */}
-
-        {usersArray.map((user)=>{
-
-            return(
-                <>
-                <div>
-
-                { tweetsArray.filter(tweet=>tweet.userId === user.id).map((tweet)=>{
-                        return(
-                            <>
-                            <div>
-                                 <div> {user.username}</div>
-                                <img src={user.imgUrl} alt=''/>
-                                </div>
-                                 {/* <div>{tweet?.tweet}</div> */}
-                                 </>
-                        )
-                })}
-                </div>
-                <div>
-              {/* {user.id === tweetUserId[0]  && <div>{user.username}</div>} */}
-              </div>
-              </>
-            )
-
-        })}
-
         {/* <img src= {userz?.imgUrl} alt=''/> */}
         {/* <div>{userz?.username}</div> */}
         <div>{tweetz?.tweet}</div>
         <img src={tweetz?.imgUrl} alt=''/>
         <EditTweet  tweetTweet={tweetz?.tweet} tweetImg={tweetz?.imgUrl} tweetId={tweetz?.id}/>
-        <div >
-           <button  onClick={handleDeleteButton}>Delete Tweet</button>
-        </div>
-        {/* { commentsArray.filter(comment=> comment?.tweetId === tweetss?.id).map((comment)=>{
 
-            return(
-                <>
-       <div key={comment.id}>{comment?.comment}</div>
-       </>
-            )
-        })} */}
-          <CreateComment  tweetId={tweetz?.id}/>
+
          { commentsArray.map((comment)=>{
 
 return(
     <>
-
-<div key={comment.id}>{comment?.comment}</div>
-
+{tweetz.id === comment.tweetId && <div key={comment.id}>{comment?.comment}</div>}
 </>
 )
 })}
-        <CommentDisplay tweetId={tweetss?.id}/>
-    
+        {/* <CommentDisplay tweetId={tweetss?.id}/> */}
 
 
       </>
