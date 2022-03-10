047f4ca8941f1d43014bceee0e33bf731336449d
Max <maxtgrigg@gmail.com>
2022-03-09 16:56:24 -0800

    usersPage bio username and pic

--- frontend/src/components/UsersPage/index.js
+++ frontend/src/components/UsersPage/index.js
@@ -14,16 +14,77 @@ import { useState } from "react";
 import CommentFunctions from "../CommentFunctions";
 
 const UsersPage = () => {
-  const dispatch = useDispatch();
-  const history = useHistory();
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-  const { userId } = useParams();
+    const {userId}= useParams();
+
+
+  // grab ALL users
+    const users = useSelector(state => state?.users)
+    const usersArray = Object.values(users)
+
+    const currentUser = useSelector(state => state.session.user);
+    const currentUserId = currentUser?.id
+
+
+
+  function userCard(user) {
+
+    if (user === undefined) return;
+    const {username, imgUrl, bio} = user;
+    return (
+      <>
+        <div className='userInfoSingleTweetPageWrapper'>
+            <div>
+        <img src={imgUrl} alt=''  className='userProfilePicOnSingleTweetPage' onError={(event) => event.target.style.display = 'none'}/>
+        </div>
+        <div className='singletweetUserNameWrapper'>
+
+          <div>@{username}</div>
+          <i class="fas fa-check-circle" id='checkMark'></i>
+
+          </div>
+
+
+         </div>
+         <br></br>
+
+          <div className='singletweetBioNameWrapper'>{bio}</div>
+
+      </>
+    );
+  }
+
+
+
+    useEffect(() => {
+        (async()=>{
+            await dispatch(getTweets());
+            await dispatch(getUsers());
+            
+        })()
+    }, [dispatch])
 
 
 
   return (
     <>
-<div id='hello'>hello</div>
+
+
+
+
+{usersArray.filter(user=>user.id==userId).map(user=>{
+
+return(
+    <>
+
+{userCard(user)}
+
+   </>
+)
+
+})}
     </>
   );
 };
